0c70be859113fe644cbaaeef653a95d03d43a838
k <ktjh@163.com>
2021-05-20 09:14:00 +0800

    新建的项目提交

--- src/main/java/com/control/Reg.java
+++ src/main/java/com/control/Reg.java
@@ -29,7 +29,7 @@ public class Reg {
     @RequestMapping("ts")
     public String tst(Model req){
         req.addAttribute("ss","fff");
-        System.out.println("reg....666");
+        System.out.println("reg....666s");
         return "/index.jsp";
     }
 
